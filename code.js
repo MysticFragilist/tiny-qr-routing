be5ae3d7594aa10bfb7cf0a62b2fc0ee6fef21fc
Samuel Montambault <mont.samuel@outlook.com>
2021-09-08 01:16:13 -0400

    verify navigator user agent for android/iOS

--- code.js
+++ code.js
@@ -1,8 +1,15 @@
+var appStore = "https://apps.apple.com/ca/app/%C3%A9tsmobile/id557463461?l=fr"
+var playStore = "https://play.google.com/store/apps/details?id=ca.etsmtl.applets.etsmobile"
+
 var isAndroid = /(android)/i.test(navigator.userAgent)
+var isIos = navigator.userAgent.match(/ipad|iphone|ipod/i)
 
 if(isAndroid) {
-  console.log('is Android')
+  window.location = playStore
+}
+else if(isIos) {
+  window.location = appStore
 }
 else {
-  console.log('is not Android')
+  window.location = playStore
 }
